1f08c7dee31f651c30073f86e9d3d62899b896dc
iqrahy <iqrahyderxwave@gmail.com>
2024-09-06 12:42:25 +0500

    form validation complete and sign up page creation

---
+++ pages/signin.js
@@ -0,0 +1,29 @@
+document.getElementById('userForm').addEventListener('submit', async function(e){
+
+    e.preventDefault();
+
+    let email = document.getElementById('exampleFormControlInput1').value;
+    let password = document.getElementById('inputPassword').value;
+
+    try {
+        
+        let USER_API = await fetch('https://fakestoreapi.com/users');
+
+        let users = await USER_API.json();
+
+        const user = users.find(user => user.email === email && user.password === password);
+    
+
+        if(user){
+            window.location.href = '../index.html';
+        } else{
+            alert('Email or Password is incorrect!');
+        }
+
+    } catch (error) {
+        console.error('Error:', error)
+        alert('Something went wrong. Please try again later.')
+    }
+
+});
+
